535a564925a18413bf16e4a1c26c86fa22d11811
Patrick Merchant <patrickmerchant9@gmail.com>
2023-01-08 16:14:34 +0000

    add createDeck test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { createDeck } from "./lib/utils";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("deck setup tests", () => {
+  test("inits a deck of 52 cards", () => {
+    const deck = createDeck();
+    expect(deck).toHaveLength(52);
+    console.log(deck);
+  });
 });
