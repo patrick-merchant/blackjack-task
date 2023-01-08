f2cb247e60ff43bf658ce9077ab2f77c61c92db0
Patrick Merchant <patrickmerchant9@gmail.com>
2023-01-08 17:51:01 +0000

    add player state and basic functions to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Game } from "./containers/game";
 
 function App() {
   return (
     <div>
       <h1>Blackjack App</h1>
+      <Game />
     </div>
   );
 }
---
+++ src/containers/game/index.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { shuffle, createDeck } from "../../lib/utils";
+import { Player } from "../player";
+import { ICard } from "../../lib/types";
+
+export const Game = () => {
+  const [deck, setDeck] = useState(shuffle(createDeck()));
+  const [playerCount, setPlayerCount] = useState<number>(0);
+  const [playerCards, setPlayerCards] = useState<ICard[]>([]);
+  const [isPlayerBust, setIsPlayerBust] = useState<boolean>(false);
+
+  const drawPlayerCard = (numOfCards = 1) => {
+    setDeck((deck) => deck.slice(0, deck.length - numOfCards));
+    const nextCards = deck.slice(deck.length - numOfCards, deck.length);
+    setPlayerCards((prevPlayerCards) => [...prevPlayerCards, ...nextCards]);
+    console.log(playerCards);
+  };
+
+  const startRound = () => {
+    setPlayerCards([]);
+    setPlayerCount(0);
+    setIsPlayerBust(false);
+    drawPlayerCard(2);
+  };
+  const handleStartRound = () => {
+    startRound();
+  };
+
+  useEffect(() => {
+    let count = 0;
+    for (let i = 0; i < playerCards.length; i++) {
+      count += playerCards[i].weight;
+    }
+    setPlayerCount(count);
+  }, [playerCards]);
+
+  return (
+    <>
+      <button onClick={handleStartRound}>Start Round</button>
+      <Player
+        playerCards={playerCards}
+        setPlayerCards={setPlayerCards}
+        playerCount={playerCount}
+        setPlayerCount={setPlayerCount}
+        deck={deck}
+        setDeck={setDeck}
+        isPlayerBust={isPlayerBust}
+        setIsPlayerBust={setIsPlayerBust}
+        drawPlayerCard={drawPlayerCard}
+      />
+    </>
+  );
+};
